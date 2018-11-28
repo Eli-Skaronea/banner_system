2420990299a62553bc5ae70c205a4a9b1f9e8b43
eli-skaronea <eskaronea@gmail.com>
2018-11-28 00:51:20 -0700

    Changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920235516) do
+ActiveRecord::Schema.define(version: 20180929061440) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,34 @@ ActiveRecord::Schema.define(version: 20180920235516) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["course_id"], name: "index_sections_on_course_id"
+  end
+
+  create_table "sections_students", force: :cascade do |t|
+    t.integer  "section_id"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_sections_students_on_section_id"
+    t.index ["student_id"], name: "index_sections_students_on_student_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
